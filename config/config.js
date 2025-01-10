1a0cc43b731b0405b70de5539256edc44b15cefa
Harold Carazas <car23068@byui.edu>
2025-01-10 16:13:22 -0500

    feat: rest file and * for cors added

--- config/config.js
+++ config/config.js
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 export const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/cse341';
-export const CORS_ALLOWED = ['http://127.0.0.1:5500'];
+export const CORS_ALLOWED = ['http://127.0.0.1:5500', '*'];
