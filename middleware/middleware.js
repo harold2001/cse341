83d97fca16be729d0559de57d21a43bee5513140
Harold Carazas <car23068@byui.edu>
2025-01-20 13:00:12 -0500

    fix: debug on prod finished

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,7 +2,6 @@ import { CORS_ALLOWED } from '../config/config.js';
 
 export const validateCORS = (req, res, next) => {
   const { origin } = req.headers;
-  console.log('origin', origin);
   if (CORS_ALLOWED.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin ?? '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, accept');
