656b360116acc362ead3f07a97956efc8112773c
Harold Carazas <car23068@byui.edu>
2025-01-20 12:52:55 -0500

    debug: origin in middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,9 +2,10 @@ import { CORS_ALLOWED } from '../config/config.js';
 
 export const validateCORS = (req, res, next) => {
   const { origin } = req.headers;
+  console.log('origin', origin);
   if (CORS_ALLOWED.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin ?? '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, accept');
     next();
   } else {
     res.status(403).send('CORS not allowed');
